af43e0317f96aaf6c0130c75882f6034d4acd137
Juan M. Barrios <juan.barrios@conabio.gob.mx>
2016-10-18 10:59:48 -0500

    Se agrega el verbo de getSpecie/{id}

--- methods/verbs.js
+++ methods/verbs.js
@@ -1,8 +1,8 @@
 var pg = require('pg');
+var moment = require('moment');
 var config = require('../config.js');
 
 var pool = new pg.Pool(config.db);
-
 
 /**
  * Return the value of param `name` when present or `defaultValue`.
@@ -47,11 +47,14 @@ var getParam = function(req, name, defaultValue) {
  *
  */
 
-exports.getGridIds = function(req, res) {
+exports.getGridIds = function(req, res, next) {
   pool.query('SELECT gridid FROM public.sp_grid_terrestre', 
              function(err, result) {
-               if(err) res.sendStatus(500).status(err.message);
-               res.json({'data': result.rows});
+               if(err) {
+                 next(err);
+                } else {
+                 res.json({'data': result.rows});
+                }
              });
 };
 
@@ -69,7 +72,7 @@ exports.getGridIds = function(req, res) {
  *
  */
 
-exports.getSpecie = function(req, res) {
+exports.getSpecie = function(req, res, next) {
   var specie_name = getParam(req, 'nom_sp');
   var limit = getParam(req, 'limit', 20);
   if (specie_name) {
@@ -93,8 +96,62 @@ exports.getSpecie = function(req, res) {
   }
   pool.query(queryConfig, function(err, result) {
     if (err) {
-      res.status(500);
-      res.json({'err': err.message});
+      next(err);
     } else res.json({'data': result.rows});
     });
+};
+
+/**
+ * infoSpecie regresa GeoJson con las coordenadas de las ocurrencias de la
+ * especie además de información adicional sobre la información de las 
+ * observaciones.
+ *
+ * @param {express.Request} req
+ * @param {express.Response} res
+ *
+ */
+
+exports.infoSpecie = function(req, res, err) {
+  var specie_id = req.params.specieId;
+  var fecha_incio = moment(getParam(req, 'fechaincio','1500'),
+                           ['YYYY-MM-DD', 'YYYY-MM', 'YYYY'], 'es');
+  var fecha_fin = moment(getParam(req, 'fechafin', Date.now()),
+                         ['YYYY-MM-DD', 'YYYY-MM', 'YYYY'], 'es');
+  var sin_fecha = Number(getParam(req, 'sfecha', 1));
+  
+  var queryConfig = {
+    text: "WITH grid_data AS(" +
+          "  SELECT geom AS grid_geom, gridid FROM sp_grid_terrestre WHERE animalia || plantae ||" +
+		      "    fungi || protoctista || prokaryotae  || animalia_exoticas || plantae_exoticas || " +
+		      "    fungi_exoticas || protoctista_exoticas || prokaryotae_exoticas @> ARRAY[$1::integer]" +
+          "  ), " +
+          " grid_occ AS ("+
+	        "   SELECT count(*) AS occ_cell FROM grid_data" +
+          "  ) " +
+          " SELECT DISTINCT st_asgeojson(the_geom) AS json_geom, gridid, entid, occ_cell, " +
+          "  urlejemplar, fechacolecta FROM public.snib JOIN grid_data ON ST_Contains(grid_geom, the_geom), " +
+          "  grid_occ WHERE spid = $1 ORDER BY gridid;",
+    values: [specie_id]
+  };
+
+  pool.query(queryConfig, function(err, result) {
+    if (err) {
+      next(err);
+    } else {
+      var qresults = result.rows;
+      qresults.map(function(e){
+        e.json_geom = JSON.parse(e.json_geom);
+        var fechacolecta = moment(e.fechacolecta, 
+                                  ["YYYY-MM-DD", "YYYY-MM", "YYYY"], 'es')
+        if (fechacolecta.isBetween(fecha_incio, fecha_fin)) {
+          e.discarded = 0;
+        } else if (sin_fecha && !(fechacolecta.isValid())) {
+          e.discarded = 0;
+        } else {
+          e.discarded = 1;
+        }
+      });
+
+      res.json({'data': qresults});}
+  });
 };
