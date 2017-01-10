a8e82c8d68e72d2981aa5c72061c015da4995109
Juan Carlos Salazar <sacj.88@gmail.com>
2017-01-10 00:47:04 -0600

    redireccion a desarrollo

--- config.js
+++ config.js
@@ -2,22 +2,22 @@
 
 var config = {}
 
-// config.db = {}
-// config.db.database = process.env.DBNAME || 'snib'
-// config.db.user = process.env.DBUSER || 'demouser'
-// config.db.password = process.env.DBPWD || 'demopass'
-// config.db.host = process.env.DBHOST || 'snib.conabio.gob.mx'
-// config.db.port = process.env.DBPORT || '5434'
-// config.db.application_name = 'expressMiddleware'
-// // Configure pool of connections
-
 config.db = {}
 config.db.database = process.env.DBNAME || 'snib'
-config.db.user = process.env.DBUSER || 'postgres'
-config.db.password = process.env.DBPWD || 'conabio2008'
-config.db.host = process.env.DBHOST || '172.16.1.198'
-config.db.port = process.env.DBPORT || '5435'
+config.db.user = process.env.DBUSER || 'demouser'
+config.db.password = process.env.DBPWD || 'demopass'
+config.db.host = process.env.DBHOST || 'snib.conabio.gob.mx'
+config.db.port = process.env.DBPORT || '5434'
 config.db.application_name = 'expressMiddleware'
+// Configure pool of connections
+
+// config.db = {}
+// config.db.database = process.env.DBNAME || 'snib'
+// config.db.user = process.env.DBUSER || 'postgres'
+// config.db.password = process.env.DBPWD || 'conabio2008'
+// config.db.host = process.env.DBHOST || '172.16.1.198'
+// config.db.port = process.env.DBPORT || '5435'
+// config.db.application_name = 'expressMiddleware'
 
 
 config.db.poolSize = 10
