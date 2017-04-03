4d302574604033012116a3b95fde1d39a1298803
Juan M. Barrios <juan.barrios@conabio.gob.mx>
2017-04-03 10:11:41 -0500

    Se documentaron las rutas de niche
    Se uso jsdoc para documentar las rutas de niche en el archivo
    `nicherouter.js`. Además se agregó jsdoc como dependencia para
    generar la documentación con ese paquete.

--- routes/nicherouter.js
+++ routes/nicherouter.js
@@ -1,6 +1,28 @@
+/** Express router que proveé las rutas asociadas a niche
+ * @module routes/nicherouter
+ * @requires express
+ */
+
+
+/**
+ * Express router que monta las funciones asociadas a niche. 
+ * @type {object}
+ * @const
+ * @namespace nicheRouter
+ */
 var router = require('express').Router()
 var verbsCtrl = require('../controllers/verbsniche')
 
+
+/**
+ * Ruta que muestra un mensaje de bienvenida 
+ * @name all/
+ * @function
+ * @memberof module:routes/nicherouter~nicheRouter
+ * @inner
+ * @param {string} path - Express path
+ * @param {callback} middleware - Express middleware.
+ */
 router.all('/', function(req, res) {
   res.json(
     { data: { 
@@ -9,124 +31,170 @@ router.all('/', function(req, res) {
   )
 })
 
-/**************************************************************************************************************************/
-/**************************************************************************************************************************/
-/**************************************************************************************************************************/
-/************************************************************* VERBOS PARA EL NUEVO SERVIDOR ******************************/
+/*** VERBOS PARA NICHO ***/
+
+/**
+ * Ruta que calcula el score entre las variables elegidas. 
+ * @name get/getGeoRel
+ * @function
+ * @memberof module:routes/nicherouter~nicheRouter
+ * @inner
+ * @param {string} path - Express path
+ * @param {callback} middleware - Express middleware.
+ */
+router.route('/getGeoRel')
+  .get(
+    verbsCtrl.getGeoRelNiche_VT,
+    verbsCtrl.getGeoRelNiche_V,
+    verbsCtrl.getGeoRelNiche_T,
+    verbsCtrl.getGeoRelNiche    
+  )
+  .post(
+    verbsCtrl.getGeoRelNiche_VT,
+    verbsCtrl.getGeoRelNiche_V,
+    verbsCtrl.getGeoRelNiche_T,
+    verbsCtrl.getGeoRelNiche
+  )
 
 
-/************************************************************* VERBOS PARA NICHO ******************************/
-
-  router.route('/getGeoRel')
-    .get(
-      verbsCtrl.getGeoRelNiche_VT,
-      verbsCtrl.getGeoRelNiche_V,
-      verbsCtrl.getGeoRelNiche_T,
-      verbsCtrl.getGeoRelNiche    
-    )
-    .post(
-      verbsCtrl.getGeoRelNiche_VT,
-      verbsCtrl.getGeoRelNiche_V,
-      verbsCtrl.getGeoRelNiche_T,
-      verbsCtrl.getGeoRelNiche
-    )
-
-  
-
-  router.route('/getFreq')
-    .get(
-      verbsCtrl.getFreqNiche_VT,
-      verbsCtrl.getFreqNiche_V,
-      verbsCtrl.getFreqNiche_T,
-      verbsCtrl.getFreqNiche    
-    )
-    .post(
-      verbsCtrl.getFreqNiche_VT,
-      verbsCtrl.getFreqNiche_V,
-      verbsCtrl.getFreqNiche_T,
-      verbsCtrl.getFreqNiche 
-    )
-
-  
-  router.route('/getFreqMap')
-    .get(
-      // verbsCtrl.getFreqMap_TM,
-      // verbsCtrl.getFreqMap_TA,
-      verbsCtrl.getFreqMapNiche_M,
-      verbsCtrl.getFreqMapNiche_A,
-      verbsCtrl.getFreqMapNiche_T,
-      verbsCtrl.getFreqMapNiche    
-    )
-    .post(
-      // verbsCtrl.getFreqMap_TM,
-      // verbsCtrl.getFreqMap_TA,
-      verbsCtrl.getFreqMapNiche_M,
-      verbsCtrl.getFreqMapNiche_A,
-      verbsCtrl.getFreqMapNiche_T,
-      verbsCtrl.getFreqMapNiche 
-    )
+/**
+ * Ruta que obtiene las frecuencia de epsilon y score por especie.
+ * @name get/getFreq
+ * @function
+ * @memberof module:routes/nicherouter~nicheRouter
+ * @inner
+ * @param {string} path - Express path
+ * @param {callback} middleware - Express middleware.
+ */
+router.route('/getFreq')
+  .get(
+    verbsCtrl.getFreqNiche_VT,
+    verbsCtrl.getFreqNiche_V,
+    verbsCtrl.getFreqNiche_T,
+    verbsCtrl.getFreqNiche    
+  )
+  .post(
+    verbsCtrl.getFreqNiche_VT,
+    verbsCtrl.getFreqNiche_V,
+    verbsCtrl.getFreqNiche_T,
+    verbsCtrl.getFreqNiche 
+  )
 
 
-
-  router.route('/getFreqCelda')
-    .get(
-      // verbsCtrl.getFreqCelda_VTA,
-      // verbsCtrl.getFreqCelda_VA,
-      // verbsCtrl.getFreqCelda_VT,
-      // verbsCtrl.getFreqCelda_TA,
-      verbsCtrl.getFreqCeldaNiche_A,
-      verbsCtrl.getFreqCeldaNiche_V,
-      verbsCtrl.getFreqCeldaNiche_T,
-      verbsCtrl.getFreqCeldaNiche    
-    )
-    .post(
-      // verbsCtrl.getFreqCelda_VTA,
-      // verbsCtrl.getFreqCelda_VA,
-      // verbsCtrl.getFreqCelda_VT,
-      // verbsCtrl.getFreqCelda_TA,
-      verbsCtrl.getFreqCeldaNiche_A,
-      verbsCtrl.getFreqCeldaNiche_V,
-      verbsCtrl.getFreqCeldaNiche_T,
-      verbsCtrl.getFreqCeldaNiche 
-    )
-
-
-  router.route('/getScoreDecil')
-    .get(
-      // verbsCtrl.getScoreDecil_VTA,
-      // verbsCtrl.getScoreDecil_VT,
-      // verbsCtrl.getScoreDecil_VA,
-      // verbsCtrl.getScoreDecil_TA,
-      verbsCtrl.getScoreDecilNiche_A,
-      verbsCtrl.getScoreDecilNiche_V,
-      verbsCtrl.getScoreDecilNiche_T,
-      verbsCtrl.getScoreDecilNiche   
-    )
-    .post(
-      // verbsCtrl.getScoreDecil_VTA,
-      // verbsCtrl.getScoreDecil_VT,
-      // verbsCtrl.getScoreDecil_VA,
-      // verbsCtrl.getScoreDecil_TA,
-      verbsCtrl.getScoreDecilNiche_A,
-      verbsCtrl.getScoreDecilNiche_V,
-      verbsCtrl.getScoreDecilNiche_T,
-      verbsCtrl.getScoreDecilNiche   
-    )
+/**
+ * Ruta que obtiene la suma del score por celda.
+ * @name get/getFreqMap
+ * @function
+ * @memberof module:routes/nicherouter~nicheRouter
+ * @inner
+ * @param {string} path - Express path
+ * @param {callback} middleware - Express middleware.
+ */
+router.route('/getFreqMap')
+  .get(
+    // verbsCtrl.getFreqMap_TM,
+    // verbsCtrl.getFreqMap_TA,
+    verbsCtrl.getFreqMapNiche_M,
+    verbsCtrl.getFreqMapNiche_A,
+    verbsCtrl.getFreqMapNiche_T,
+    verbsCtrl.getFreqMapNiche    
+  )
+  .post(
+    // verbsCtrl.getFreqMap_TM,
+    // verbsCtrl.getFreqMap_TA,
+    verbsCtrl.getFreqMapNiche_M,
+    verbsCtrl.getFreqMapNiche_A,
+    verbsCtrl.getFreqMapNiche_T,
+    verbsCtrl.getFreqMapNiche 
+  )
 
 
-  router.route('/getGridSpecies')
-    .get(
-      verbsCtrl.getGridSpeciesNiche_M,
-      verbsCtrl.getGridSpeciesNiche_A,
-      verbsCtrl.getGridSpeciesNiche_T,
-      verbsCtrl.getGridSpeciesNiche        
-    )
-    .post(
-      verbsCtrl.getGridSpeciesNiche_M,
-      verbsCtrl.getGridSpeciesNiche_A,
-      verbsCtrl.getGridSpeciesNiche_T,
-      verbsCtrl.getGridSpeciesNiche        
-    )
+/**
+ * Ruta que obtiene la frecuencia del score por celda.
+ * @name get/getFreqCelda
+ * @function
+ * @memberof module:routes/nicherouter~nicheRouter
+ * @inner
+ * @param {string} path - Express path
+ * @param {callback} middleware - Express middleware.
+ */
+router.route('/getFreqCelda')
+  .get(
+    // verbsCtrl.getFreqCelda_VTA,
+    // verbsCtrl.getFreqCelda_VA,
+    // verbsCtrl.getFreqCelda_VT,
+    // verbsCtrl.getFreqCelda_TA,
+    verbsCtrl.getFreqCeldaNiche_A,
+    verbsCtrl.getFreqCeldaNiche_V,
+    verbsCtrl.getFreqCeldaNiche_T,
+    verbsCtrl.getFreqCeldaNiche    
+  )
+  .post(
+    // verbsCtrl.getFreqCelda_VTA,
+    // verbsCtrl.getFreqCelda_VA,
+    // verbsCtrl.getFreqCelda_VT,
+    // verbsCtrl.getFreqCelda_TA,
+    verbsCtrl.getFreqCeldaNiche_A,
+    verbsCtrl.getFreqCeldaNiche_V,
+    verbsCtrl.getFreqCeldaNiche_T,
+    verbsCtrl.getFreqCeldaNiche 
+  )
+
+
+/**
+ * Ruta que obtiene los deciles del score 
+ * @name get/getScoreDecil
+ * @function
+ * @memberof module:routes/nicherouter~nicheRouter
+ * @inner
+ * @param {string} path - Express path
+ * @param {callback} middleware - Express middleware.
+ */
+router.route('/getScoreDecil')
+  .get(
+    // verbsCtrl.getScoreDecil_VTA,
+    // verbsCtrl.getScoreDecil_VT,
+    // verbsCtrl.getScoreDecil_VA,
+    // verbsCtrl.getScoreDecil_TA,
+    verbsCtrl.getScoreDecilNiche_A,
+    verbsCtrl.getScoreDecilNiche_V,
+    verbsCtrl.getScoreDecilNiche_T,
+    verbsCtrl.getScoreDecilNiche   
+  )
+  .post(
+    // verbsCtrl.getScoreDecil_VTA,
+    // verbsCtrl.getScoreDecil_VT,
+    // verbsCtrl.getScoreDecil_VA,
+    // verbsCtrl.getScoreDecil_TA,
+    verbsCtrl.getScoreDecilNiche_A,
+    verbsCtrl.getScoreDecilNiche_V,
+    verbsCtrl.getScoreDecilNiche_T,
+    verbsCtrl.getScoreDecilNiche   
+  )
+
+
+/**
+ * Ruta que obtiene el score por celda agrupado por decil
+ * @name get/getGridSpecies
+ * @function
+ * @memberof module:routes/nicherouter~nicheRouter
+ * @inner
+ * @param {string} path - Express path
+ * @param {callback} middleware - Express middleware.
+ */
+router.route('/getGridSpecies')
+  .get(
+    verbsCtrl.getGridSpeciesNiche_M,
+    verbsCtrl.getGridSpeciesNiche_A,
+    verbsCtrl.getGridSpeciesNiche_T,
+    verbsCtrl.getGridSpeciesNiche        
+  )
+  .post(
+    verbsCtrl.getGridSpeciesNiche_M,
+    verbsCtrl.getGridSpeciesNiche_A,
+    verbsCtrl.getGridSpeciesNiche_T,
+    verbsCtrl.getGridSpeciesNiche        
+  )
 
 
 /************************************************************* VERBOS PARA REDES ******************************/
--- server.js
+++ server.js
@@ -1,40 +1,39 @@
 // server.js
 
 // call the packages we need
-var express = require('express');
-var cors = require('cors');
-var bodyParser = require('body-parser');
-var debug = require('debug');
+var express = require('express')
+var cors = require('cors')
+var bodyParser = require('body-parser')
+var debug = require('debug')
 
-var log = debug('snib-middleware:log');
-var error = debug('snib-middleware:error');
-var app = express();
+var log = debug('snib-middleware:log')
+var error = debug('snib-middleware:error')
+var app = express()
 
-app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
 
 var port = process.env.PORT || 8080        // set our port
 
-// console.log("port: " + port);
+// console.log("port: " + port)
 
 // Routes for our api
-var verbsRouter = require('./routes/router');
-var newVerbsRouter = require('./routes/nicherouter')
+var verbsRouter = require('./routes/router')
+var nicheRouter = require('./routes/nicherouter')
 
 // Register our routes
 // all of our routes will be prefixed with /snib
 app.use('/snib', verbsRouter)
-app.use('/niche', newVerbsRouter)
+app.use('/niche', nicheRouter)
 
 // Start the server
 var server = app.listen(port, function () {
-  var port = server.address().port;
+  var port = server.address().port
   
-  console.log('Aplicación corriendo en el puerto %s', port);
-  log('Aplicación corriendo en el puerto %s', port);
+  log('Aplicación corriendo en el puerto %s', port)
 
-});
+})
 
 // error handling
 if (app.get('env') === 'development') {
@@ -58,4 +57,4 @@ app.use(function (err, req, res, next) {
     })
 })
 
-module.exports = server;
+module.exports = server
