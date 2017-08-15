abeb8eac40fbb431606c5f9a9964487fbe6d50f3
Juan Carlos Salazar <sacj.88@gmail.com>
2017-08-15 00:32:03 -0500

    cambio de tabla temp a permanente y cambio de procesos sincronos a asincronos

--- config.js
+++ config.js
@@ -2,25 +2,15 @@
 
 var config = {}
 
-// config.db = {}
-// config.db.database = process.env.DBNAME || 'niche'
-// config.db.user = process.env.DBUSER || 'postgres'
-// config.db.password = process.env.DBPWD || 'conabio2008'
-// config.db.host = process.env.DBHOST || 'niche2.conabio.gob.mx'
-// config.db.port = process.env.DBPORT || '5439'
-// config.db.application_name = 'expressMiddleware'
+config.db = {}
+config.db.database = process.env.DBNAME 
+config.db.user = process.env.DBUSER 
+config.db.password = process.env.DBPWD 
+config.db.host = process.env.DBHOST 
+config.db.port = process.env.DBPORT 
+config.db.application_name = 'expressMiddleware'
 
-// config.db = {}
-// config.db.database = process.env.DBNAME 
-// config.db.user = process.env.DBUSER 
-// config.db.password = process.env.DBPWD 
-// config.db.host = process.env.DBHOST 
-// config.db.port = process.env.DBPORT 
-// config.db.application_name = 'expressMiddleware'
-
- 
 // Configure pool of connections
-
 config.db.poolSize = 10
 
 module.exports = config
