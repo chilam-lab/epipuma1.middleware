a5455abf41c8b027768d0d85c53c916ca73fccd6
Juan Carlos Salazar <sacj.88@gmail.com>
2017-10-03 09:27:57 -0500

    tabla decil

--- controllers/sql/getScoreDecil/get_score_decil_RaA.sql
+++ controllers/sql/getScoreDecil/get_score_decil_RaA.sql
@@ -1,11 +1,13 @@
 select 
 	--* 
-	 decil, avg(l_sup) as l_sup, avg(l_inf) as l_inf, avg(sum) as sum, avg(avg) as avg, avg(vp) as vp, avg(fn) as fn, avg(nulos) as nulos, avg(recall) as recall -- , avg(iter) as iter
+	 decil, avg(l_sup) as l_sup, avg(l_inf) as l_inf, avg(sum) as sum, avg(avg) as avg, 
+	 array_agg(arraynames) as arraynames,
+	 avg(vp) as vp, avg(fn) as fn, avg(nulos) as nulos, avg(recall) as recall -- , avg(iter) as iter
 from iteratevalidationprocessbydecil($<iterations>, $<spid>, $<N>, $<alpha>, $<min_occ>, array[$<discardedDeleted:raw>]::int[], '$<res_celda_sp:raw>', '$<res_celda_snib:raw>', '$<res_celda_snib_tb:raw>', '', '$<where_config_raster:value>', 'abio', $<filter_time>, $<caso>, $<lim_inf>, $<lim_sup>, '$<fossil:value>', '$<idtabla:value>', true)
 --from iteratevalidationprocessbydecil(1, 28923, 94544, 0.01, 0, array[]::int[], 'cells_16km', 'gridid_16km', 'grid_16km_aoi', 'where clasevalida = ''Mammalia'' ', '', 'bio', false, -1, 2010, 2020, '', 'temp_01')
 -- 27332 28923
 -- where layer = ''bio01'' 
 where decil is not null 
- group by decil
+ group by decil, arraynames
  order by decil desc
 
