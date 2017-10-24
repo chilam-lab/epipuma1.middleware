19a30d729c938aff745782c102a1afe024d009b4
Juan Carlos Salazar <sacj.88@gmail.com>
2017-10-24 00:21:51 -0500

    correccion abioticas label en comunidad

--- controllers/sql/getNet/get_nodes_raster_bio.sql
+++ controllers/sql/getNet/get_nodes_raster_bio.sql
@@ -4,8 +4,9 @@ with source AS (
 			layer as reinovalido, label as phylumdivisionvalido, tag as clasevalida, ''::text as  ordenvalido, ''::text as familiavalida, ''::text as generovalido,
 			case when type = 1 then
 			layer
-			else
-			(label || ' ' || tag) 
+			ELSE
+			(label || ' ' || round(cast(split_part(split_part(tag,':',1),'.',1) as numeric)/10,2)  ||' ºC - ' || round(cast(split_part(split_part(tag,':',2),'.',1) as numeric)/10,2) || ' ºC')
+			--(label || ' ' || tag) 
 			end as especievalidabusqueda,
 			1 as grp,
 			$<res_celda:raw> AS cells 
