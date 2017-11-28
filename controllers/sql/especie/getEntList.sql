8754f7580f4275ed1a6b9b5550f743decce5fd43
Juan Carlos Salazar <sacj.88@gmail.com>
2017-11-28 00:04:01 -0600

    ajuste ocurrencias

--- controllers/sql/especie/getEntList.sql
+++ controllers/sql/especie/getEntList.sql
@@ -1,8 +1,15 @@
-select 	$<columnas:raw>
-		--reinovalido, phylumdivisionvalido, clasevalida, ordenvalido, familiavalida, generovalido, especievalidabusqueda
+select 	
+		$<columnas:raw>,
+		--reinovalido, phylumdivisionvalido, clasevalida, ordenvalido, familiavalida, generovalido, especievalidabusqueda,
+		--distinct ordenvalido,
+		--icount(array[]::int[]) as occ
+		icount($<res_celda_sp:raw>) as occ
 from sp_snib
-where   lower($(nivel:raw)) like lower($<str>||'%')
-		--lower(especievalidabusqueda) like lower('lynx%')
+where   
+		lower($(nivel:raw)) like lower($<str>||'%')
+		--lower(ordenvalido) like lower('carn%')
 $<limite:raw>
+--limit 15
+
 --select row_to_json(c,true) as ent from ( select nom_sp, spid, reinovalido, phylumdivisionvalido, clasevalida, ordenvalido, familiavalida, generovalido, epitetovalido from sp_snib where lower(especievalidabusqueda) like lower('lynx%') limit 30 offset 0) c
 --select row_to_json(c,true) as ent from (select distinct clasevalida as clasevalida from sp_snib where lower(clasevalida) like lower('mamma%') limit 15 offset 0) c
