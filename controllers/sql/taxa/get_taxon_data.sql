34dad21bdaa742f1c6ded43074c8ec8d4ac649c2
Raul Sierra <raul.sierra@gconabio.gob.mx>
2018-01-21 13:09:51 -0600

    Get taxon data

--- controllers/sql/taxa/get_taxon_data.sql
+++ controllers/sql/taxa/get_taxon_data.sql
@@ -2,8 +2,8 @@
 * @Author: Raul Sierra
 * @Date:   2017-12-01 10:45:31
 * @Last Modified by:   Raul Sierra
-* @Last Modified time: 2017-12-01 11:25:34
+* @Last Modified time: 2017-12-01 12:03:30
 */
 SELECT spid, especievalidabusqueda as valid_name
 FROM sp_snib
-WHERE spid = $<spid> 
+WHERE spid = $<spid> AND especievalidabusqueda <> ''
