adfd7fc05f50370cff0284f2739def10ad040480
Raul Sierra <raul.sierra@gconabio.gob.mx>
2018-01-30 15:25:20 -0600

    Arregla query de celdas, agrega prueba de endpoint escuchando y mensaje de bienvenida del endpoit

--- controllers/sql/getCells/get_taxon_cells.sql
+++ controllers/sql/getCells/get_taxon_cells.sql
@@ -2,7 +2,7 @@
 * @Author: Raul Sierra
 * @Date:   2017-10-26 14:20:30
 * @Last Modified by:   Raul Sierra
-* @Last Modified time: 2017-10-30 12:42:21
+* @Last Modified time: 2018-01-25 16:33:43
 */
 /*getTaxonCells*/
 
@@ -10,14 +10,7 @@
 SELECT $<res_celda:raw> AS cell_id, $<tax_level:raw>, MAX(aniocolecta) as max_year, MIN(aniocolecta) as min_year, count(*) as num_records
   FROM snib
   WHERE $<tax_level:raw> = $<tax_name> AND
-  	$<res_celda:raw> IS NOT NULL AND
-  	ejemplarfosil = ANY(CASE 
-  						WHEN NOT $<fossil> THEN ARRAY['']
-  						ELSE ARRAY['SI','']
-  						END) AND
-  	aniocolecta <> (CASE 
-  						WHEN NOT $<sfecha> THEN 9999
-  						ELSE -1
-  						END) AND
-  	aniocolecta >= $<start_year> AND aniocolecta <= $<end_year>
+  	($<res_celda:raw> IS NOT NULL) AND
+    ($<fossil> OR ejemplarfosil <> 'SI') AND
+    ($<sfecha:raw> OR aniocolecta is not null) 
   GROUP BY cell_id, $<tax_level:raw>
--- routes/taxarouter.js
+++ routes/taxarouter.js
@@ -2,18 +2,35 @@
 * @Author: Raul Sierra
 * @Date:   2017-11-28 15:47:33
 * @Last Modified by:   Raul Sierra
-* @Last Modified time: 2017-12-01 09:38:51
+* @Last Modified time: 2018-01-24 11:33:04
 */
 var router = require('express').Router()
 
 /**
- * Express router que monta las funciones asociadas a redes. 
+ * Express router que monta las funciones para obtener info sobre taxa. 
  * @type {object}
  * @const
- * @namespace netRouter
+ * @namespace taxaRouter
  */
 var router = require('express').Router()
 var taxa_controller = require('../controllers/taxa')
+
+/**
+ * Ruta que muestra un mensaje de bienvenida 
+ * @name all/
+ * @function
+ * @memberof module:routes/taxarouter~taxaRouter
+ * @inner
+ * @param {string} path - Express path
+ * @param {callback} middleware - Express middleware.
+ */
+router.all('/', function(req, res) {
+  res.json(
+    { data: { 
+      message: 'Hola, este endpoint contiene servicios para obtener info taxonomica'
+    }}
+  )
+})
 
 /**
  * Ruta que obtiene las aristas de las relaciones 
