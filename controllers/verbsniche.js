caf07bb82771ba2caca72abdfd4cd8825af62135
Juan Carlos Salazar <sacj.88@gmail.com>
2018-02-19 16:51:57 -0600

    eliminación de parametros no utilizados y error de selección de celdas

--- controllers/verbsniche.js
+++ controllers/verbsniche.js
@@ -540,10 +540,16 @@ exports.getValidationTables = function (req, res, next) {
       var iter = getParam(req, 'iter')
       var idtbl =  "tbl_" + new Date().getTime() //getParam(req, 'idtable')
       var iter = getParam(req, 'iterations',iterations)
+
+
+      var grid_resolution = verb_utils.getParam(req, 'grid_res',16)
+      var res_celda_sp =  "cells_"+grid_resolution+"km"   
+      var res_celda_snib =  "gridid_"+grid_resolution+"km" 
+      var res_celda_snib_tb = "grid_"+grid_resolution+"km_aoi" 
       
-      var res_celda_sp = verb_utils.getParam(req, 'res_celda_sp', 'cells_16km')
-      var res_celda_snib = verb_utils.getParam(req, 'res_celda_snib', 'gridid_16km')
-      var res_celda_snib_tb = verb_utils.getParam(req, 'res_celda_snib_tb', 'grid_16km_aoi')
+      // var res_celda_sp = verb_utils.getParam(req, 'res_celda_sp', 'cells_16km')
+      // var res_celda_snib = verb_utils.getParam(req, 'res_celda_snib', 'gridid_16km')
+      // var res_celda_snib_tb = verb_utils.getParam(req, 'res_celda_snib_tb', 'grid_16km_aoi')
       
 
       pool.any(queries.getValidationTables.createTables, {
