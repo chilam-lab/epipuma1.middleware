669609e085d93e7d461824453281c22f3d39fa16
Raul Sierra <raul.sierra@gconabio.gob.mx>
2018-02-21 18:16:42 -0600

    Trabajando seccion de calculo de scores de celda para evaluar registros de especies

--- controllers/sql/taxa/get_taxon_children.sql
+++ controllers/sql/taxa/get_taxon_children.sql
@@ -2,11 +2,11 @@
 * @Author: Raul Sierra
 * @Date:   2017-11-29 11:26:02
 * @Last Modified by:   Raul Sierra
-* @Last Modified time: 2017-11-29 13:13:35
+* @Last Modified time: 2018-02-20 15:07:16
 */
 
 /* getTaxonChildren */
 
-SELECT $<root_level:raw> as root_level, $<child_level:raw> as scientific_name
+SELECT $<root_level:raw> as root_level, $<child_level:raw> as scientific_name, spid
 FROM sp_snib
 WHERE $<root_level:raw> = $<root_name> AND $<child_level:raw> <> ''
