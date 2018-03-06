6c5884ed5ab4db60c90d082b889ed1bd0765d4ab
Juan Carlos Salazar <sacj.88@gmail.com>
2018-03-06 12:26:27 -0600

    avance prueba verbos

--- test/group_test.js
+++ test/group_test.js
@@ -8,10 +8,10 @@ function importTest(name, path) {
 
 describe("group_test", function () {
     beforeEach(function () {
-       console.log("running group test");
+       console.log("running individual test");
     });
-    importTest("specie", './specie/specie_info');
-    importTest("raster", './raster/raster_info');
+    importTest("cells", './cells/cell_info');
+    importTest("model", './model/model');
     after(function () {
         console.log("group tests finished");
     });
