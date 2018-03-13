793e0131b2a1e880a1805507913f00992d71d00f
Juan Carlos Salazar <sacj.88@gmail.com>
2018-03-12 21:24:28 -0600

    error en resolucion de malla y ocurrencias de especie

--- controllers/sql/especie/getSpecies.sql
+++ controllers/sql/especie/getSpecies.sql
@@ -1,17 +1,21 @@
 SELECT DISTINCT st_asgeojson(the_geom) as json_geom, 
-				$<res_celda:raw> as gridid,
+				$<res_celda_snib:raw> as gridid,
 				--gridid_16km as gridid, 
 				urlejemplar, 
-				fechacolecta
+				fechacolecta,
+				--icount(sp_snib.cells_32km) as occ
+				icount($<res_celda_sp:raw>) as occ
 				-- ejemplarfosil
 FROM snib 
+join sp_snib
+on snib.spid = sp_snib.spid
 WHERE 	
-		spid = $<spid> AND
-		-- spid = 28923 AND 
-		especievalidabusqueda <> '' and
-		$<res_celda:raw> is not null
-		-- gridid_16km is not null
+		snib.spid = $<spid> AND
+		--snib.spid = 27332 AND 
+		snib.especievalidabusqueda <> '' and
+		$<res_celda_snib:raw> is not null
+		--gridid_16km is not null
 		$<sfosil:raw> 
-		-- and (ejemplarfosil <> 'SI' or ejemplarfosil is null)
+		--and (ejemplarfosil <> 'SI' or ejemplarfosil is null)
 
 
