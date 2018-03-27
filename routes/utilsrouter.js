180bd93138039d7c0c78f799979c2fb61caa4097
Juan Carlos Salazar <sacj.88@gmail.com>
2018-03-27 00:01:36 -0600

    registro usuario, avance delete qtype parameter

--- routes/utilsrouter.js
+++ routes/utilsrouter.js
@@ -23,37 +23,70 @@ var verbsCtrl = require('../controllers/verbsniche')
  * @param {string} path - Express path
  * @param {callback} middleware - Express middleware.
  */
+
+
+router.route('/especie/getUserReg')
+  .get(verbsCtrl.getUserReg)
+  .post(verbsCtrl.getUserReg)
+
+router.route('/especie/setUserReg')
+  .post(verbsCtrl.setUserReg)
+
+router.route('/especie/getEntList')
+  .get(verbsCtrl.getEntListNiche)
+  .post(verbsCtrl.getEntListNiche)
+
+router.route('/especie/getToken')
+  .get(verbsCtrl.getToken)
+  .post(verbsCtrl.getToken)
+
+router.route('/especie/getValuesFromToken')
+  .get(verbsCtrl.getValuesFromToken)
+  .post(verbsCtrl.getValuesFromToken)
+
+
+router.route('/especie/getGridGeoJson')
+  .get(verbsCtrl.getGridGeoJsonNiche)
+  .post(verbsCtrl.getGridGeoJsonNiche)
+  
+
+
+  
+
+
 router.route('/especie')
   .get(
-    // verbsCtrl.getUserReg,
-    // verbsCtrl.setUserReg,
-    verbsCtrl.getValuesFromToken,
-    verbsCtrl.getToken,
+    // verbsCtrl.getValuesFromToken,
+    // verbsCtrl.getToken,
     verbsCtrl.getValidationTables,
     verbsCtrl.processValidationTables,
     verbsCtrl.deleteValidationTables,
-    verbsCtrl.getGridGeoJsonNiche,
+    // verbsCtrl.getGridGeoJsonNiche,
     verbsCtrl.getVariablesNiche,
     verbsCtrl.getRasterNiche,
     verbsCtrl.getCountGridid,  
     verbsCtrl.getGrididsNiche,  
-    verbsCtrl.getSpeciesNiche,
-    verbsCtrl.getEntListNiche
+    verbsCtrl.getSpeciesNiche
+    // verbsCtrl.getEntListNiche
+    // verbsCtrl.getUserReg
+    // verbsCtrl.setUserReg
   )
   .post(
-    // verbsCtrl.getUserReg,
-    verbsCtrl.getValuesFromToken,
-    verbsCtrl.getToken,
+    // verbsCtrl.getValuesFromToken,
+    // verbsCtrl.getToken,
     verbsCtrl.getValidationTables,
     verbsCtrl.processValidationTables,
     verbsCtrl.deleteValidationTables,
-    verbsCtrl.getGridGeoJsonNiche,
+    // verbsCtrl.getGridGeoJsonNiche,
     verbsCtrl.getVariablesNiche,
     verbsCtrl.getRasterNiche,
     verbsCtrl.getCountGridid,  
     verbsCtrl.getGrididsNiche,  
-    verbsCtrl.getSpeciesNiche,
-    verbsCtrl.getEntListNiche
+    verbsCtrl.getSpeciesNiche
+    // verbsCtrl.getEntListNiche
+    // verbsCtrl.getUserReg,
+    // verbsCtrl.setUserReg
   )
+
 
 module.exports = router
