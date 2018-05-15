143c52c5327f2c96a8afbe06b501205cdc19ab95
Juan Carlos Salazar <sacj.88@gmail.com>
2018-05-14 21:47:47 -0500

    avance getscoredeciltables

--- controllers/sql/raster/raster_bios.sql
+++ controllers/sql/raster/raster_bios.sql
@@ -1,8 +1,15 @@
-SELECT 
-	label, 
-	layer, 
-	type 
-FROM raster_bins
-WHERE type = 0 
-GROUP BY label, layer, type 
-ORDER BY layer
+-- SELECT 
+-- 	label, 
+-- 	layer, 
+-- 	type 
+-- FROM raster_bins
+-- WHERE type = 0 
+-- GROUP BY label, layer, type 
+-- ORDER BY layer
+SELECT rb.layer, rbf.label, rbf."type"
+FROM raster_bins as rb
+join raster_bins_future rbf
+on rb.bid = rbf.bid
+where rbf.type = 0
+GROUP BY rbf.label, rb.layer, rbf.type 
+ORDER BY rb.layer;
