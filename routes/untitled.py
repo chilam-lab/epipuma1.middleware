1ca4d432f912c18bed7a4886848da77e731a94f4
pedro <zs100115@gmail.com>
2018-05-23 12:31:02 -0500

    Se modifica verbo getRasterVariables para que regrese el footprint_region en la que esta definida cada conjunto de variables

--- routes/untitled.py
+++
@@ -1,5 +0,0 @@
-occ_data = pd.read_table(
-            'oc.txt',
-            usecols=list(gbiftosnib.GBIF_TO_SNIBGEOPORTAL.keys()),
-            dtype=gbiftosnib.GBIF_DATA_TYPES_IMPORT,
-            )
