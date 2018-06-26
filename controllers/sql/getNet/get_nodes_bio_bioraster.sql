1b3369dce5e1cf5585547d0ea40021a4d68fc011
Juan Carlos Salazar <sacj.88@gmail.com>
2018-06-25 21:27:27 -0500

    ajuste de variables climaticas por region en comunidad

--- controllers/sql/getNet/get_nodes_bio_bioraster.sql
+++ controllers/sql/getNet/get_nodes_bio_bioraster.sql
@@ -8,29 +8,49 @@ with source AS (
 	$<where_config_source:raw>	 
 	and especievalidabusqueda <> ''
 ),
+raster_cell as (
+	SELECT 
+		bid as spid,
+		layer as reinovalido, label as phylumdivisionvalido, tag as clasevalida, ''::text as  ordenvalido, ''::text as familiavalida, ''::text as generovalido,
+		case when type = 1 then
+			layer
+		else
+			case when strpos(label,'Precipit') = 0 then
+			(layer || ' ' || round(cast(split_part(split_part(tag,':',1),'.',1) as numeric)/10,2)  ||' ºC - ' || round(cast(split_part(split_part(tag,':',2),'.',1) as numeric)/10,2) || ' ºC')
+			else
+			(layer || ' ' || round(cast(split_part(split_part(tag,':',1),'.',1) as numeric),2)  ||' mm - ' || round(cast(split_part(split_part(tag,':',2),'.',1) as numeric),2) || ' mm')
+			end
+		end as especievalidabusqueda,
+		2 as grp,
+		unnest($<res_celda:raw>) as cell
+		--unnest(cells_16km) as cell
+	FROM raster_bins
+	$<where_config_target_raster:raw>
+	--where layer = 'bio1'
+),
 target AS (
 	SELECT spid,
 	 		reinovalido, phylumdivisionvalido, clasevalida, ordenvalido, familiavalida, generovalido, especievalidabusqueda,
 	 		2 as grp,
+	 		--cells_16km as cells
 			$<res_celda:raw> AS cells 
 	FROM sp_snib 
 	--WHERE generovalido = 'Lutzomyia'
 	$<where_config_target:raw>	 
 	and especievalidabusqueda <> ''
 	union
-	SELECT  bid as spid,
-			layer as reinovalido, label as phylumdivisionvalido, tag as clasevalida, ''::text as  ordenvalido, ''::text as familiavalida, ''::text as generovalido,
-			case when type = 1 then
-			layer
-			ELSE
-			(label || ' ' || round(cast(split_part(split_part(tag,':',1),'.',1) as numeric)/10,2)  ||' ºC - ' || round(cast(split_part(split_part(tag,':',2),'.',1) as numeric)/10,2) || ' ºC')
-			--(label || ' ' || tag) 
-			end as especievalidabusqueda,
-			2 as grp,
-			$<res_celda:raw> AS cells 
-	FROM raster_bins
-	--where layer = 'bio01'
-	$<where_config_target_raster:raw>
+	SELECT  spid,
+			reinovalido, phylumdivisionvalido, clasevalida, ordenvalido, familiavalida, generovalido, especievalidabusqueda, grp,
+			array_agg(rc.cell) as cells 
+	FROM raster_cell as rc
+	--join grid_16km_aoi as gdkm
+	join $<res_celda_snib_tb:raw> as gdkm
+	--on rc.cell = gdkm.gridid_16km
+	on rc.cell = gdkm.$<res_celda_snib:raw>
+	join america
+	on st_intersects(america.geom, gdkm.small_geom)
+	where america.country = 'MEXICO'
+	group by spid, reinovalido, phylumdivisionvalido, clasevalida, ordenvalido, familiavalida, generovalido, especievalidabusqueda, grp
 )
 select 	spid,
 	 	reinovalido, phylumdivisionvalido, clasevalida, ordenvalido, familiavalida, generovalido, 
