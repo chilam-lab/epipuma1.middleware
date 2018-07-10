7d5e1ef24e833ab9ab0e12304de3b7f61de6f7c5
Juan Carlos Salazar <sacj.88@gmail.com>
2018-07-09 23:51:22 -0500

    avance analisis por region

--- controllers/sql/grids/grid_xxkm.sql
+++ controllers/sql/grids/grid_xxkm.sql
@@ -1,6 +1,18 @@
+with arg_gids as (
+	select array_agg(gid) as arg_ids 
+	FROM aoi
+	--where fgid = 28 
+	where fgid = $<id_country:raw>
+	group by fgid
+)
 SELECT
 	json::json
+	--footprint_region, gids
 FROM
-	grid_geojson_$<grid_res>km_aoi
+	--grid_geojson_16km_aoi, arg_gids
+	grid_geojson_$<grid_res>km_aoi, arg_gids
 WHERE
-	footprint_region=$<region>
+	gids @> arg_ids
+	-- se descarta opcion de MX y US al mismo tiempo
+	and footprint_region <> 3
+	-- footprint_region=$<id_country>
