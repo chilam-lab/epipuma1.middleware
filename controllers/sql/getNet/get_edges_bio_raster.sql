b4ec3fab6208271b241b5a9a8a6f1972af5aa7bb
pedro <zs100115@gmail.com>
2018-07-23 19:53:34 -0500

    Se agrega parametro de region para servicio de comunidad en getEdgesNiche

--- controllers/sql/getNet/get_edges_bio_raster.sql
+++ controllers/sql/getNet/get_edges_bio_raster.sql
@@ -1,7 +1,12 @@
 /*getGeoRel sin filtros*/
 with source AS (
 	SELECT  spid,
-			$<res_celda:raw> AS cells 
+			array_intersection($<res_celda:raw>,
+				ARRAY(SELECT cells
+					FROM grid_geojson_$<resolution:raw>km_aoi
+					WHERE footprint_region = $<region:raw>
+				)
+			) AS cells 
 	FROM sp_snib 
 	--WHERE generovalido = 'Aedes'
 	$<where_config_source:raw>	 
@@ -9,7 +14,12 @@ with source AS (
 ),
 target AS (
 	SELECT  bid as spid,
-			$<res_celda:raw> AS cells 
+			array_intersection($<res_celda:raw>,
+				ARRAY(SELECT cells
+					FROM grid_geojson_$<resolution:raw>km_aoi
+					WHERE footprint_region = $<region:raw>
+				)
+			) AS cells 
 	FROM raster_bins
 	--where layer = 'bio01'
 	$<where_config_target_raster:raw>	  
