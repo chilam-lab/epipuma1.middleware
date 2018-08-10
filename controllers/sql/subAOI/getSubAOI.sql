f111171ecc33041bbbbaecfb67556685f83d828f
pedro <zs100115@gmail.com>
2018-08-10 14:13:50 -0500

    Se modifica servicio para llenar el combo box de regiones con las regiones definidas en materialized views

--- controllers/sql/subAOI/getSubAOI.sql
+++ controllers/sql/subAOI/getSubAOI.sql
@@ -1,5 +1,18 @@
-SELECT 
-	footprint_region::int,
-	border::json
-FROM
-	grid_geojson_64km_aoi
+WITH t1 AS (
+	SELECT DISTINCT footprint_region, country, fgid 
+	FROM (	SELECT a.footprint_region, b.country, b.fgid 
+			FROM grid_geojson_64km_aoi AS a 
+			JOIN aoi AS b 
+			ON b.gid = ANY(a.gid)) AS t0 
+			ORDER BY footprint_region
+),
+regions AS (
+	SELECT footprint_region
+	FROM grid_geojson_64km_aoi
+)
+SELECT DISTINCT t1.footprint_region, 
+				(SELECT string_agg(t1.country::varchar, '; ') AS country FROM t1 WHERE t1.footprint_region = r.footprint_region),
+				(SELECT t1.fgid FROM t1 WHERE t1.footprint_region = r.footprint_region LIMIT 1 )
+FROM regions AS r
+JOIN t1
+ON t1.footprint_region = r.footprint_region;
