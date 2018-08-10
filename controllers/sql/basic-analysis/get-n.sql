c6caab476c03918a90ac688495ed1ea6c50f3396
pedro <zs100115@gmail.com>
2018-08-10 15:06:09 -0500

    Se modifica verbo getSpecies para analisis por region

--- controllers/sql/basic-analysis/get-n.sql
+++ controllers/sql/basic-analysis/get-n.sql
@@ -1,14 +1,3 @@
---SELECT icount(cells) as n
---FROM grid_geojson_${grid_resolution:raw}km_aoi
---where footprint_region = ${footprint_region:raw}
-with available_countries as(
-	SELECT country, fgid, array_agg(gid) as gids
-	FROM aoi
-	group by fgid, country
-)
-select fgid as id_country, icount(cells) as n
-from available_countries
-join grid_geojson_${grid_resolution:raw}km_aoi
-on available_countries.gids @> grid_geojson_${grid_resolution:raw}km_aoi.gid
+SELECT icount(cells) as n
+FROM grid_geojson_${grid_resolution:raw}km_aoi
 where footprint_region = ${footprint_region:raw}
-order by fgid
--- controllers/sql/especie/getSpecies.sql
+++ controllers/sql/especie/getSpecies.sql
@@ -1,40 +1,47 @@
 with spid_occ as (
 	select 
 		-- icount(array_agg(distinct gridid_16km)) as occ
-		icount(array_agg(distinct $<res_celda_snib:raw>)) as occ
-	FROM snib 
-	join aoi
-	on snib.gid = aoi.gid
+		icount(array_agg(distinct a.$<res_celda_snib:raw>)) as occ
+	FROM snib AS a
+	JOIN (
+		SELECT UNNEST(gid) AS gid 
+		--FROM grid_geojson_64km_aoi
+		FROM ${res_celda_snib_tb:raw}
+		--WHERE footprint_region=1 
+		WHERE footprint_region=${region}
+		) AS b
+	ON a.gid = b.gid
 	WHERE 	
-			--aoi.fgid = 19 and
-			aoi.fgid = $<id_country:raw> and
-			--snib.spid = 27333 and
-			snib.spid = $<spid> AND
-			snib.especievalidabusqueda <> '' and
-			--gridid_16km is not null
-			$<res_celda_snib:raw> is not null
-			$<sfosil:raw> 
-			--and (ejemplarfosil <> 'SI' or ejemplarfosil is null)
-	group by spid
-)
-SELECT DISTINCT st_asgeojson(the_geom) as json_geom, 
-				--gridid_16km as gridid,
-				$<res_celda_snib:raw> as gridid,
-				urlejemplar, 
-				--fechacolecta,
-				aniocolecta,
-				spid_occ.occ as occ
-FROM snib 
-join aoi
-on snib.gid = aoi.gid,
-spid_occ
-WHERE 	
-		--aoi.fgid = 19 and
-		aoi.fgid = $<id_country:raw> and
+		a.spid = $<spid> AND
 		--snib.spid = 27333 and
-		snib.spid = $<spid> AND
-		snib.especievalidabusqueda <> '' and
+		a.especievalidabusqueda <> '' and
 		--gridid_16km is not null
 		$<res_celda_snib:raw> is not null
 		$<sfosil:raw> 
 		--and (ejemplarfosil <> 'SI' or ejemplarfosil is null)
+	GROUP BY a.spid
+)
+SELECT DISTINCT st_asgeojson(a.the_geom) as json_geom, 
+				--gridid_16km as gridid,
+				a.$<res_celda_snib:raw> as gridid,
+				a.urlejemplar, 
+				--fechacolecta,
+				a.aniocolecta,
+				(SELECT occ FROM spid_occ) as occ
+FROM snib AS a
+JOIN (
+	SELECT UNNEST(gid) AS gid 
+	--FROM grid_geojson_64km_aoi
+	FROM ${res_celda_snib_tb:raw}
+	--WHERE footprint_region=1 
+	WHERE footprint_region=${region}
+	) AS b
+ON a.gid = b.gid
+WHERE 	
+	--snib.spid = 27333 and
+	a.spid = $<spid> AND
+	a.especievalidabusqueda <> '' and
+	--gridid_16km is not null
+	$<res_celda_snib:raw> is not null
+	$<sfosil:raw> 
+	--and (ejemplarfosil <> 'SI' or ejemplarfosil is null)
