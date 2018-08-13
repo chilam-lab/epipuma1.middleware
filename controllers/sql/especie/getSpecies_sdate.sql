2ae71b978da201c868b010c580dbede000bcda6d
pedro <zs100115@gmail.com>
2018-08-13 15:18:38 -0500

    Se modifican las queries de getSpecies con distintos filtros

--- controllers/sql/especie/getSpecies_sdate.sql
+++ controllers/sql/especie/getSpecies_sdate.sql
@@ -1,41 +1,48 @@
 with spid_occ as (
-	select 
+	SELECT 
 		--icount(array_agg(distinct gridid_16km)) as occ
-		icount(array_agg(distinct $<res_celda_snib:raw>)) as occ
-	FROM snib 
-	join aoi
-	on snib.gid = aoi.gid
+		icount(array_agg(distinct a.$<res_celda_snib:raw>)) as occ
+	FROM snib AS a
+	JOIN (
+		SELECT UNNEST(gid) AS gid 
+		--FROM grid_geojson_64km_aoi
+		FROM ${res_celda_snib_tb:raw}
+		--WHERE footprint_region=1 
+		WHERE footprint_region=${region}
+		) AS b
+	ON a.gid = b.gid
 	WHERE 	
-			--aoi.fgid = 19 and
-			aoi.fgid = $<id_country:raw> and
-			--snib.spid = 27333 and
-			snib.spid = $<spid> AND
-			snib.especievalidabusqueda <> '' and
-			$<res_celda_snib:raw> is not null
-			--gridid_16km is not null
-			$<sfosil:raw> 
-			--and (ejemplarfosil <> 'SI' or ejemplarfosil is null)
-	group by spid
+		a.spid = $<spid> AND
+		--a.spid = 27333 and
+		a.especievalidabusqueda <> '' and
+		$<res_celda_snib:raw> is not null
+		--gridid_16km is not null
+		$<sfosil:raw> 
+		--and (ejemplarfosil <> 'SI' or ejemplarfosil is null)
+	GROUP BY a.spid
 )
-SELECT DISTINCT st_asgeojson(the_geom) as json_geom, 
-				$<res_celda:raw> as gridid,
+SELECT DISTINCT st_asgeojson(a.the_geom) as json_geom, 
+				a.$<res_celda:raw> as gridid,
 				--gridid_16km as gridid,
-				urlejemplar,
+				a.urlejemplar,
 				--fechacolecta,
-				aniocolecta,
-				spid_occ.occ as occ
-FROM snib 
-join aoi
-on snib.gid = aoi.gid,
-spid_occ
+				a.aniocolecta,
+				(SELECT occ FROM spid_occ) as occ
+FROM snib AS a
+JOIN (
+	SELECT UNNEST(gid) AS gid 
+	--FROM grid_geojson_64km_aoi
+	FROM ${res_celda_snib_tb:raw}
+	--WHERE footprint_region=1 
+	WHERE footprint_region=${region}
+	) AS b
+ON a.gid = b.gid
 WHERE 	
-		--aoi.fgid = 19 and
-		aoi.fgid = $<id_country:raw> and
-		--snib.spid = 27333 and
-		snib.spid = $<spid> AND
-		snib.especievalidabusqueda <> '' and
-		$<res_celda_snib:raw> is not null and
+		--a.spid = 27333 and
+		a.spid = $<spid> AND
+		a.especievalidabusqueda <> '' and
+		a.$<res_celda_snib:raw> is not null and
 		--gridid_16km is not null and
-		aniocolecta is not null and aniocolecta <> 9999
+		a.aniocolecta is not null and aniocolecta <> 9999
 		$<sfosil:raw> 
 		--and (ejemplarfosil <> 'SI' or ejemplarfosil is null)
