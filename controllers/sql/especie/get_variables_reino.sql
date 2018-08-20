a79f2cd6fe6bf6717710eb89c48b2b1f4752713d
pedro <zs100115@gmail.com>
2018-08-20 13:59:24 -0500

    Se agrega filtro de region a getVariables

--- controllers/sql/especie/get_variables_reino.sql
+++ controllers/sql/especie/get_variables_reino.sql
@@ -1,4 +1,8 @@
-select distinct $<taxon> as name, count(*) spp 
-from sp_snib 
-where $<taxon> <> '' and especievalidabusqueda <> '' 
-group by reinovalido order by $<taxon>
+SELECT DISTINCT a.$<taxon> AS name, count(*) AS spp 
+FROM sp_snib AS a
+WHERE a.$<taxon> <> '' 
+	AND a.especievalidabusqueda <> ''
+	AND  array_length( array_intersection(ARRAY(SELECT cells 
+		FROM grid_geojson_64km_aoi
+		WHERE footprint_region=$<region>)::integer[], a.cells_64km), 1) > 0
+GROUP BY a.reinovalido, a.$<taxon>
