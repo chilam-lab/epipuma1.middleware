57e15464685b3bfeb88ef9b8d32503c3ef3da786
Pedro R <zs10011598@gmail.com>
2020-07-09 21:12:32 -0500

    verbo getGridSpeciesTaxon muestra distribucion de clase target con datos de entrenamiento

--- controllers/sql/especie/get_grid_species_taxons.sql
+++ controllers/sql/especie/get_grid_species_taxons.sql
@@ -8,6 +8,13 @@ gids_countries AS (
 	SELECT UNNEST(gid) AS gid 
 	FROM $<resolution_view:raw>
 	WHERE footprint_region=$<region:raw>
+),
+first_period AS (
+	SELECT DISTINCT a.$<gridid:raw> AS gridid, 1 fp
+	FROM $<snib_grid_xxkm:raw> AS a 
+	JOIN spids_species AS b
+	ON a.spid = b.spid
+	WHERE $<where_filter_first:raw>
 )
 SELECT distinct a.$<gridid:raw> AS gridid, sum(a.occ) AS occ
 FROM $<snib_grid_xxkm:raw> AS a 
@@ -15,6 +22,9 @@ JOIN spids_species AS b
 ON a.spid = b.spid
 JOIN gids_countries AS c
 ON a.gid = c.gid
-WHERE 	a.$<gridid:raw> is not null
+LEFT JOIN first_period AS d
+ON a.$<gridid:raw> = d.gridid
+WHERE 	d.fp is null AND 
+		a.$<gridid:raw> is not null
 		$<where_filter:raw> 
 GROUP BY a.$<gridid:raw>
