8ea79d023612213e82c158947c69a854ea2a91d4
Pedro Romero <pedro.romero@bluestart.mx>
2021-03-24 20:41:23 -0600

    correccion en primer perido en query de letalidad

--- controllers/sql/time-validation/get_count_cells_first_lethality.sql
+++ controllers/sql/time-validation/get_count_cells_first_lethality.sql
@@ -53,6 +53,7 @@ WITH positivos as (
 				  a.diacolecta <> 99 and
 				  a.diacolecta <> -1 and
 				  ((generovalido = 'COVID-19' AND especieepiteto = 'FALLECIDO')) and 
+				  '${lim_inf_first:raw}' <= make_date(a.aniocolecta, a.mescolecta, a.diacolecta) and 
 				  make_date(a.aniocolecta, a.mescolecta, a.diacolecta) < '${lim_sup_first:raw}'
 			GROUP BY a.gridid_${grid_resolution:raw}km
 		) AS b
